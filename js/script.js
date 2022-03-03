1eef6ca3301b1c5b9414fc563717e974867153ac
victoria mwangi <victoriamwangi021@gmail.com>
2022-03-03 10:47:18 +0300

    feat:calculate day of the week

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var btn = document.getElementById("btn");
+
+// Day of the week(d) = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) mod 7
+btn.addEventListener("click", function (e) {
+    e.preventDefault()
+    var stringCentury = String(document.getElementById("yob").value).slice(0, 2);
+    var stringYear = String(document.getElementById("yob").value).slice(2, 4);
+    var century = parseInt(stringCentury);
+    var year = parseInt(stringYear);
+    var month = parseInt(document.getElementById('month').value);
+    var day = parseInt(document.getElementById('day').value);
+    var weekday = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
+    console.log(weekday);
+
+    // var weekDay = function (century, year, month, day) {
+    // }
+    // console.log(weekDay())
+    var weekd = ((((19 / 4) - 2 * 19 - 1) + ((5 * 98 / 4)) + ((26 * (4 + 1) / 10)) + 28) % 7);
+
+    console.log(weekd)
+})
+
