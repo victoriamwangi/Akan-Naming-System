014ddc2a6ec6ba4ef4d89c6daf7668aaf8ded11b
victoria mwangi <victoriamwangi021@gmail.com>
2022-03-06 10:14:33 +0300

    feat: calculate day of week

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 var btn = document.getElementById("btn");
 var form = document.getElementById('form');
 
-
-// Day of the week(d) = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) mod 7
 btn.addEventListener("click", function (e) {
     e.preventDefault()
-    var stringCentury = String(document.getElementById("yob").value).slice(0, 2);
-    var stringYear = String(document.getElementById("yob").value).slice(2, 4);
-    var century = parseInt(stringCentury);
-    var year = parseInt(stringYear);
+    var year = parseInt(document.querySelector("#yob").value);
     var month = parseInt(document.getElementById('month').value);
     var day = parseInt(document.getElementById('day').value);
     var gender = document.querySelector("#gender").value;
 
 
-
-    // Determine and display Akan Name and day of birth 
     function khanName() {
         var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
         var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
@@ -23,8 +16,6 @@ btn.addEventListener("click", function (e) {
         var fName;
         var mName;
         var modTitle;
-        var floatDay = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
-        var numDay = Math.trunc(floatDay);
         var charDay;
         var dayOutput;
         for (fName = 0; fName < femaleNames.length; fName++) {
@@ -34,8 +25,9 @@ btn.addEventListener("click", function (e) {
 
         }
 
+        var numDay = (day + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7;
         //Validate date inputs and display outputs
-        if (month > 12 || day > 31 || month < 1 || day < 1 || month == "" || day == "" || century == "") {
+        if (month > 12 || day > 31 || month < 1 || day < 1 || month == "" || day == "" || year == "") {
             message = "Kindly input correct dates!";
             modErrTitle = "Oops";
             form.querySelector('#exampleModalLabel').innerText = modErrTitle;
@@ -99,15 +91,9 @@ btn.addEventListener("click", function (e) {
             dayOutput = "You were born on a " + charDay;
             form.querySelector('#exampleModalLabel').innerText = modSuccTitle;
             form.querySelector('#dayOutput').innerText = dayOutput;
-
-
         };
-
-
-
     };
     khanName();
-
 });
 
 
